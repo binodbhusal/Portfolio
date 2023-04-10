680c8c3bb367e757aaf67ad14dd2838cad189045
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-04-10 19:37:00 +0100

    Completes mobile-menu with JS file

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const headline = document.querySelector('.headline');
+const headline = document.querySelector('#main-area');
 const menuBar = document.querySelector('.nav-item');
 const menuLogo = document.getElementById('vector');
 const mylogo = document.querySelector('.mylogo');
 const closeBtn = document.getElementById('btnclose');
+const listItems = document.querySelectorAll('.mobile-menu > li');
+
+for (let i = 0; i <= listItems.length - 1; i += 1) {
+  listItems[i].addEventListener('click', () => {
+    menuBar.style.visibility = 'hidden';
+    headline.style.filter = 'blur(0)';
+    mylogo.style.filter = 'blur(0)';
+    menuLogo.style.visibility = 'visible';
+  });
+}
 
 menuLogo.addEventListener('click', () => {
   menuBar.style.visibility = 'visible';
   menuLogo.style.visibility = 'hidden';
-  headline.style.filter = 'blur(2px)';
+  menuBar.style.filter = 'blur(0)';
+  headline.style.filter = 'blur(4px)';
   mylogo.style.filter = 'blur(2px)';
 });
 closeBtn.addEventListener('click', () => {
@@ -15,7 +26,5 @@ closeBtn.addEventListener('click', () => {
   headline.style.filter = 'blur(0)';
   mylogo.style.filter = 'blur(0)';
   menuLogo.style.visibility = 'visible';
-  menuBar.style.transition = '0.5s';
+  menuBar.style.transition = '0.2s';
 });
-
-
